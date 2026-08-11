6648be7ee86165ea3bbc6f2b63f251b94c64a2c3
Rajnish singh <rajnishsingh99503@gmail.com>
2026-08-11 22:26:18 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"https://three-interviewiq-6client-nxyw.onrender.com/",
+    origin:"https://three-interviewiq-6client-nxyw.onrender.com",
     credentials:true
 }))
 
